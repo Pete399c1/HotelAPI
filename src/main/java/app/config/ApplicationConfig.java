43f7364bbee0bfb15cd09912c22906a3bf342829
Peter399c1 <cph-pm260@cphbusiness.dk>
2025-09-27 14:41:31 +0200

    start

---
+++ src/main/java/app/config/ApplicationConfig.java
@@ -0,0 +1,34 @@
+package app.config;
+
+
+/*
+import app.routes.PoemRoutes;
+import app.routes.Routes;
+import io.javalin.Javalin;
+import io.javalin.config.JavalinConfig;
+*/
+public class ApplicationConfig {
+    /*
+    private static PoemRoutes routes = new PoemRoutes();
+
+
+    public static void configuration(JavalinConfig config){
+        config.showJavalinBanner = false;
+        config.bundledPlugins.enableRouteOverview("/routes");
+        config.router.contextPath = "/api"; // base path for all endpoints
+        config.router.apiBuilder(routes);
+    }
+
+    public static Javalin startServer(int port) {
+        routes = new Routes();
+        var app = Javalin.create(ApplicationConfig::configuration);
+        app.start(port);
+        return app;
+    }
+
+    public static void stopServer(Javalin app) {
+        app.stop();
+    }
+     */
+
+}
